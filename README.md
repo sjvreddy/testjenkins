9e1e3e4327755ff2ff42f39efff4e9acf314c937
sjvreddy <sudhir@chef.io>
2017-10-19 09:20:18 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# testjenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: testjenkins
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
