4a2d899a2a0df86e22ff9b9d5a43af1123776a49
sjvreddy <sudhir@chef.io>
2017-11-08 06:49:17 -0800

    Moved cookbooks into cookbooks directory

--- knife.rb
+++ knife.rb
@@ -4,4 +4,4 @@ log_location              STDOUT
 node_name                 "chefadmin"
 client_key                "#{current_dir}/chefadmin.pem"
 chef_server_url           "https://ec2-35-164-35-44.us-west-2.compute.amazonaws.com/organizations/chefinc"
-cookbook_path             ["#{current_dir}"]
+cookbook_path             ["#{current_dir}/cookbooks"]
